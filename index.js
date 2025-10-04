c224cd47b150c3242dd7e4c0c8b125e96fbbaa05
fellisChaos <nishantubuntu@gmail.com>
2025-10-04 21:02:24 +0530

    implement /joke command to send random joke from external API

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ dotenv.config();
 
 const bot = new telegramBot(process.env.TOKEN, { polling: true });
 
-bot.on("message", (option) => {
-  console.log(option);
-  bot.sendMessage(option.chat.id, `Hi, ${option.chat.first_name}`);
-});
+// bot.on("message", (option) => {
+//   console.log(option);
+//   bot.sendMessage(option.chat.id, `Hi, ${option.chat.first_name}`);
+// });
+
+bot.onText(/\/joke/, async (option)=>{
+    const response = await axios.get(
+      "https://api.freeapi.app/api/v1/public/randomjokes/joke/random"
+    );
+
+    // console.log(response?.data?.data?.content)
+
+    bot.sendMessage(
+      option.chat.id,
+      `Hey ${option.chat.first_name}, \n \nHere is your joke: \n${response?.data?.data?.content}`
+    );
+})
